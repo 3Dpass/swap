ea12c872acf8ee5e328974a2cadb364d472486f0
Mikhail Fedosov <tbs.micle@gmail.com>
2025-05-26 02:32:20 +0400

    refactor: remove network URL parameter handling

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,28 +1,14 @@
-import { Outlet, useSearchParams } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import SeoHelmet from "../components/atom/SeoHelmet";
 import Footer from "../components/organism/Footer";
 import HeaderTopNav from "../components/organism/HeaderTopNav";
-import { useEffect } from "react";
 
 const MainLayout = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  useEffect(() => {
-    const networkSession = window.localStorage.getItem("network");
-    const networkParam = searchParams.get("network");
-
-    if (networkSession && networkParam) {
-      window.localStorage.setItem("network", networkParam);
-      setSearchParams({ network: networkParam });
-    } else if (networkSession) {
-      setSearchParams({ network: networkSession });
-    }
-  }, []);
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="flex min-h-screen flex-col">
       <SeoHelmet />
       <HeaderTopNav />
-      <main className="flex-1 flex flex-col">
+      <main className="flex flex-1 flex-col">
         <Outlet />
       </main>
       <Footer />
