c06d032c7598b3331dd022301bbfa4fd32ef9c90
Mikhail Fedosov <tbs.micle@gmail.com>
2025-05-26 02:00:20 +0400

    feat(config): add FROG token icon and image file

--- src/config/tokenIcons.ts
+++ src/config/tokenIcons.ts
@@ -1,14 +1,16 @@
 // Import token icons
-import P3DIcon from '../assets/img/tokens/P3D.svg';
-import TESTIcon from '../assets/img/tokens/TEST.png';
-import defaultTokenIcon from '../assets/img/tokens/default-token.svg';
+import P3DIcon from "../assets/img/tokens/P3D.svg";
+import TESTIcon from "../assets/img/tokens/TEST.png";
+import FROGIcon from "../assets/img/tokens/FROG.png";
+import defaultTokenIcon from "../assets/img/tokens/default-token.svg";
 
 // Token symbol to icon URL mapping
 // Key - token symbol in UPPERCASE (e.g., "TEST", "P3D")
 // Value - imported icon URL
 export const TOKEN_ICONS: Record<string, string> = {
-	P3D: P3DIcon,
-	TEST: TESTIcon,
+  P3D: P3DIcon,
+  TEST: TESTIcon,
+  FROG: FROGIcon,
 };
 
 export const DEFAULT_TOKEN_ICON = defaultTokenIcon;
