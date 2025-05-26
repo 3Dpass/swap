360b7045979923fbd1bf57d696c5c4334286fcd7
Mikhail Fedosov <tbs.micle@gmail.com>
2025-05-26 23:27:45 +0400

    feat(tokens): add COW, DIA, and FOX token icons

--- src/config/tokenIcons.ts
+++ src/config/tokenIcons.ts
@@ -1,8 +1,11 @@
 // Import token icons
-import P3DIcon from "../assets/img/tokens/P3D.svg";
-import TESTIcon from "../assets/img/tokens/TEST.png";
+import COWIcon from "../assets/img/tokens/COW.png";
+import DIIcon from "../assets/img/tokens/DIA.png";
+import FOXIcon from "../assets/img/tokens/FOX.png";
 import FROGIcon from "../assets/img/tokens/FROG.png";
+import P3DIcon from "../assets/img/tokens/P3D.svg";
 import PASSIcon from "../assets/img/tokens/PASS.png";
+import TESTIcon from "../assets/img/tokens/TEST.png";
 import defaultTokenIcon from "../assets/img/tokens/default-token.svg";
 
 // Token symbol to icon URL mapping
@@ -13,6 +16,9 @@ export const TOKEN_ICONS: Record<string, string> = {
   TEST: TESTIcon,
   FROG: FROGIcon,
   PASS: PASSIcon,
+  FOX: FOXIcon,
+  COW: COWIcon,
+  DIA: DIIcon,
 };
 
 export const DEFAULT_TOKEN_ICON = defaultTokenIcon;
