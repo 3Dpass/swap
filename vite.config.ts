5388ed8cbce020cba244cf1fee0c77c92c093e9f
Mikhail Fedosov <tbs.micle@gmail.com>
2025-05-26 20:29:48 +0400

    fix: add buffer polyfill for browser compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,20 @@ import EnvironmentPlugin from "vite-plugin-environment";
 import react from "@vitejs/plugin-react";
 import mkcert from "vite-plugin-mkcert";
 import svgr from "vite-plugin-svgr";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
-  plugins: [react(), svgr(), mkcert(), EnvironmentPlugin("all")],
+  plugins: [
+    react(),
+    svgr(),
+    mkcert(),
+    EnvironmentPlugin("all"),
+    nodePolyfills({
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+    }),
+  ],
 });
